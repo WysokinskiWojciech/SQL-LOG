217be91b1bfb0d15638f146aff12d95bd3d0d31b
CERTBUD-LAB <w.wysokinski.certbud@gmail.com>
2020-10-01 07:19:26 +0200

    Wersja z zapisem nagłówków tabeli danych

---
+++ SQL LOG/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace SQL_LOG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Connect = new System.Windows.Forms.Button();
+            this.IP = new System.Windows.Forms.TextBox();
+            this.Port = new System.Windows.Forms.TextBox();
+            this.DBName = new System.Windows.Forms.TextBox();
+            this.User = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.FileName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Table = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Connect
+            // 
+            this.Connect.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Connect.Location = new System.Drawing.Point(18, 312);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(278, 60);
+            this.Connect.TabIndex = 0;
+            this.Connect.Text = "Connecttion Test";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // IP
+            // 
+            this.IP.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.IP.Location = new System.Drawing.Point(141, 37);
+            this.IP.Name = "IP";
+            this.IP.Size = new System.Drawing.Size(155, 30);
+            this.IP.TabIndex = 2;
+            this.IP.Text = "192.168.0.120";
+            // 
+            // Port
+            // 
+            this.Port.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Port.Location = new System.Drawing.Point(141, 77);
+            this.Port.Name = "Port";
+            this.Port.Size = new System.Drawing.Size(155, 30);
+            this.Port.TabIndex = 3;
+            this.Port.Text = "SQLEXPRESS";
+            // 
+            // DBName
+            // 
+            this.DBName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DBName.Location = new System.Drawing.Point(141, 117);
+            this.DBName.Name = "DBName";
+            this.DBName.Size = new System.Drawing.Size(155, 30);
+            this.DBName.TabIndex = 4;
+            this.DBName.Text = "Test";
+            // 
+            // User
+            // 
+            this.User.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.User.Location = new System.Drawing.Point(141, 157);
+            this.User.Name = "User";
+            this.User.Size = new System.Drawing.Size(155, 30);
+            this.User.TabIndex = 5;
+            this.User.Text = "PLC";
+            // 
+            // Password
+            // 
+            this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Password.Location = new System.Drawing.Point(141, 197);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(155, 30);
+            this.Password.TabIndex = 6;
+            this.Password.Text = "admin123";
+            this.Password.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(13, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Host IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(13, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(13, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "DB Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(13, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "User Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(13, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Password";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(319, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 25);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "File Name";
+            // 
+            // FileName
+            // 
+            this.FileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FileName.Location = new System.Drawing.Point(447, 41);
+            this.FileName.Name = "FileName";
+            this.FileName.Size = new System.Drawing.Size(207, 30);
+            this.FileName.TabIndex = 14;
+            this.FileName.Text = "test.txt";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(325, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(329, 54);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Create File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(13, 257);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 25);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Table";
+            // 
+            // Table
+            // 
+            this.Table.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Table.Location = new System.Drawing.Point(141, 253);
+            this.Table.Name = "Table";
+            this.Table.Size = new System.Drawing.Size(155, 30);
+            this.Table.TabIndex = 16;
+            this.Table.Text = "Table_2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 474);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Table);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.FileName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.User);
+            this.Controls.Add(this.DBName);
+            this.Controls.Add(this.Port);
+            this.Controls.Add(this.IP);
+            this.Controls.Add(this.Connect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.TextBox IP;
+        private System.Windows.Forms.TextBox Port;
+        private System.Windows.Forms.TextBox DBName;
+        private System.Windows.Forms.TextBox User;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox FileName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox Table;
+    }
+}
+
---
+++ SQL LOG/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SQL_LOG
+{
+    public partial class Form1 : Form
+    {
+        bool stan = false;
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+        }
+
+        public void Connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection  polaczenie = new SqlConnection(@"Data source=" + IP.Text + @"\" +Port.Text + ";" + "database=" + DBName.Text + ";" + "User id=" + User.Text + ";" + "Password=" + Password.Text+";");
+                polaczenie.Open();
+                MessageBox.Show("Połączono z serwerem!");
+                stan = true;
+                polaczenie.Close();
+            }
+            catch {
+                MessageBox.Show("Nie połączono z serwerem! Sprawdź dane logowania");
+                stan = false;
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a;
+            int b;
+            string line = "";
+            if (stan == true)
+            {
+                try
+                {
+                    SqlConnection connection = new SqlConnection(@"Data source=" + IP.Text + @"\" + Port.Text + ";" + "database=" + DBName.Text + ";" + "User id=" + User.Text + ";" + "Password=" + Password.Text + ";");
+                    string query = "SELECT COUNT(*)  FROM INFORMATION_SCHEMA.COLUMNS WHERE table_catalog = '" + DBName.Text + "' AND table_name = '" + Table.Text + "'";
+                    string queryString = "SELECT * FROM dbo." + Table.Text + ";";
+                    string column = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME ='" + Table.Text + "'";
+                    SqlCommand command2 = new SqlCommand(column, connection);
+                    SqlCommand command1 = new SqlCommand(query, connection);
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    connection.Open();
+                    SqlDataReader reader1 = command1.ExecuteReader();
+                    reader1.Read();
+                    a = reader1[0].ToString();
+                    b = Convert.ToInt32(a);
+                    reader1.Close();
+                    SqlDataReader reader2 = command2.ExecuteReader();
+
+                    using (System.IO.StreamWriter writer = new System.IO.StreamWriter(FileName.Text, false))
+                    {
+                        while (reader2.Read())
+                        {                     
+                            line += reader2[0].ToString();
+                            line += ";";
+                        }
+                        writer.WriteLine(line);
+                    
+                    line = "";
+                    reader2.Close();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    
+                        while (reader.Read())
+                        {
+                            line = "";
+                            for (int i = 0; i < b; i++)
+                            {
+                                line += reader[i].ToString();
+                                line += ";";
+                            }
+                            writer.WriteLine(line);
+                        }
+                        reader.Close();
+
+                    }
+                                      
+                    connection.Close();
+                    MessageBox.Show("Plik zapisano!");
+                }
+                catch
+                {
+                    MessageBox.Show("Brak połączenia!. Sprawdź poprawność pola Table ");
+                }
+            }
+            else {
+                MessageBox.Show("Sprawdź połączenie!. Wciśnij Connection Test!");
+            }
+            
+        }
+    } 
+}
